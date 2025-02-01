9ff47cbf50e1890e7b247a2e5f45e33168255fe4
reasdesign <mustafaildrm053@gmail.com>
2025-02-01 17:54:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// Yükleme Ekranı
-document.addEventListener("DOMContentLoaded", () => {
-  const loader = document.querySelector(".loader-wrapper");
-  setTimeout(() => loader.style.display = "none", 2000);
+// Menü Geçişi
+document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.getElementById("menu-toggle");
+  const nav = document.querySelector("nav ul");
+
+  menuToggle.addEventListener("click", function() {
+    nav.classList.toggle("active");
+  });
+});
+
+// Scroll Animasyonları
+window.addEventListener("scroll", function() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
 });
